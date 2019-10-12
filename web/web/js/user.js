0cd98a36dfb7ce3563b61f51a1bac08b694ade8f
Shakednai <Shakednai1@gmail.com>
2019-10-12 18:40:56 +0300

    add repos to table

--- web/web/js/user.js
+++ web/web/js/user.js
@@ -2,5 +2,36 @@
 //function() getCurrUser{$("h1").text('<%= Session["user"] %>')}
 
 $(document).ready(function() {
-    $("#currUser").text("efsef");
+    addAllRepositoriesToTable();
 });
+
+
+function addRepositoryToTable(repo){
+        var markup = "<tr><td>"+repo.name+"</td><td>"+
+            repo.activeBranch +"</td><td>" +
+            repo.numOfBranches +"</td><td>" +
+            repo.lastCommitTime +"</td><td>"+
+            repo.lastCommitMessage +"</td></tr>";
+        $("#myRepos").append(markup);
+}
+
+function addAllRepositoriesToTable() {
+    //ajax get all user repos
+    var repos = [{
+        "name": "repo1",
+        "activeBranch": "branch1",
+        "numOfBranches": 4,
+        "lastCommitTime": "12.10.2019 34:56:34",
+        "lastCommitMessage": "this is the last"
+    },
+        {
+            "name": "repo2",
+            "activeBranch": "branch2",
+            "numOfBranches": 7,
+            "lastCommitTime": "13.14.2019 35:23:33",
+            "lastCommitMessage": "this is the last second repo"
+        }];
+    for (i in repos) {
+        addRepositoryToTable(repos[i]);
+    }
+}
