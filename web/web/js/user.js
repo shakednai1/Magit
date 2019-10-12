71eaa9e91f17c5f53027f3c14b475d9194ca5de0
Shakednai <Shakednai1@gmail.com>
2019-10-12 17:58:20 +0300

    fix user name

---
+++ web/web/js/user.js
@@ -0,0 +1,6 @@
+// var getCurrUser = function(){$("#currUser").innerText(innerText"dadsg")}
+//function() getCurrUser{$("h1").text('<%= Session["user"] %>')}
+
+$(document).ready(function() {
+    $("#currUser").text("efsef");
+});
--- web/web/user.js
+++
@@ -1,5 +0,0 @@
-// var getCurrUser = function(){$("#currUser").innerText(innerText"dadsg")}
-// var getCurrUser = function(){$("#currUser").text('<%= Session["user"] %>')}
-function getCurrUser(){
-    $("#currUser").append('SuchWow')
-}
