cd74915b1cb9b023cde1386681893f3519c8d333
AlonaTyu <Alona Tyupa>
2019-10-12 18:36:48 +0300

    user name from session

--- web/web/js/user.js
+++
@@ -1,6 +0,0 @@
-// var getCurrUser = function(){$("#currUser").innerText(innerText"dadsg")}
-//function() getCurrUser{$("h1").text('<%= Session["user"] %>')}
-
-$(document).ready(function() {
-    $("#currUser").text("efsef");
-});
